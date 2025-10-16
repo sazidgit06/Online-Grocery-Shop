4bad93ef10b39a3f70caad94de187ecb621d203e
sazid <sazidtec06@gmail.com>
2025-10-16 09:03:21 +0600

    featured products done

--- js/featured.js
+++ js/featured.js
@@ -1,51 +1,49 @@
-const cartIcon = document.querySelector(".cart-icon");
-const cartTab = document.querySelector(".cart-tab");
-const closeBtn = document.querySelector(".close-btn");
-const cardList = document.querySelector(".product-grid");
-const cartList = document.querySelector(".cart-list");
-const cartTotal = document.querySelector(".cart-total");
+const carticon = document.querySelector(".cart-icon");
+const carttab = document.querySelector(".cart-tab");
+const closebtn = document.querySelector(".close-btn");
+const wrapper = document.querySelector(".product-wrapper");
+const cartlist = document.querySelector(".cart-list");
+const carttotal = document.querySelector(".cart-total")
 
+carticon.addEventListener('click', (e) => {
 
-cartIcon.addEventListener('click', (e) => {
-
-  cartTab.classList.add('cart-tab-active');
+  carttab.classList.add('cart-tab-active');
   e.preventDefault();
 
 })
-closeBtn.addEventListener('click', (e) => {
+closebtn.addEventListener('click', (e) => {
 
-  cartTab.classList.remove('cart-tab-active')
+  carttab.classList.remove('cart-tab-active')
   e.preventDefault();
 
 })
 
 
-let productList = [];
-let cartProduct = [];
+let productlist = [];
+let cartproduct = [];
 
-const updateTotals = () => {
+const updatetotals = () => {
   let totalPrice = 0;
   document.querySelectorAll('.item').forEach(item => {
-    const price = parseFloat(item.querySelector('.item-total').textContent.replace('$',''))
-    totalPrice+=price;
+    const price = parseFloat(item.querySelector('.item-total').textContent.replace('$', ''))
+    totalPrice += price;
   });
-  cartTotal.textContent = `$${totalPrice}`
+  carttotal.textContent = `$${totalPrice}`
 }
 
-const showCards = () => {
-  productList.forEach(product => {
-    const orderCard = document.createElement('div');
-    orderCard.classList.add('col')
-
-    orderCard.innerHTML = `
-        <div class='product-item'>
-            <figure>
+const showcards = () => {
+  productlist.forEach(product => {
+    const producthtml = document.createElement('div')
+    producthtml.classList.add('product-item');
+    producthtml.classList.add('swiper-slide');
+    producthtml.innerHTML = `
+                <figure>
                   <a href="index.html" title="Product Title">
                     <img src="${product.image}" alt="Product Thumbnail" class="tab-image">
                   </a>
                 </figure>
                 <div class="d-flex flex-column text-center">
-                  <h3 class="fs-6 fw-normal">${product.name}</h3>
+                  <h3 class="fs-6 fw-normal">Greek Style Plain Yogurt</h3>
                   <div>
                     <span class="rating">
                       <svg width="18" height="18" class="text-warning">
@@ -68,7 +66,7 @@ const showCards = () => {
                   </div>
                   <div class="d-flex justify-content-center align-items-center gap-2">
                     <del>$24.00</del>
-                    <span class="text-dark fw-semibold">${product.price}</span>
+                    <span class="text-dark fw-semibold">$18.00</span>
                     <span
                       class="badge border border-dark-subtle rounded-0 fw-normal px-1 fs-7 lh-1 text-body-tertiary">10%
                       OFF</span>
@@ -77,50 +75,45 @@ const showCards = () => {
                     <div class="row g-1 mt-2">
                       <div class="col-3"><input type="number" name="quantity"
                           class="form-control border-dark-subtle input-number quantity" value="1"></div>
-                      <div class="col-7">
-                      <a href="#" class="btn btn-primary rounded-1 p-2 fs-7 btn-cart myCart">
-                      <svg width="18"
+                      <div class="col-7"><a href="#" class="btn btn-primary rounded-1 p-2 fs-7 btn-cart mycart"><svg width="18"
                             height="18">
                             <use xlink:href="#cart"></use>
-                      </svg>
-                      Add to Cart</a>
-                      </div>
+                          </svg> Add to Cart</a></div>
                       <div class="col-2"><a href="#" class="btn btn-outline-dark rounded-1 p-2 fs-6"><svg width="18"
                             height="18">
                             <use xlink:href="#heart"></use>
                           </svg></a></div>
                     </div>
                   </div>
-                </div>
-                </div>
+                </div> 
         `;
 
-    cardList.appendChild(orderCard);
+    wrapper.appendChild(producthtml);
 
-    const cartBtn = orderCard.querySelector('.myCart');
+    const cartBtn = producthtml.querySelector('.mycart');
 
     cartBtn.addEventListener('click', (e) => {
       e.preventDefault();
-      addToCart(product);
+      addtocart(product);
     })
 
   })
 }
 
-const addToCart = (product) => {
+const addtocart = (product) => {
 
-  const exist = cartProduct.find(item => item.id === product.id);
-  if(exist){
+  const exist = cartproduct.find(item => item.id === product.id);
+  if (exist) {
     alert("Item already added");
     return;
   }
 
   alert('Product is added to cart');
 
-  cartProduct.push(product);
+  cartproduct.push(product);
 
   let quantity = 1;
-  let price = parseFloat(product.price.replace('$',''))
+  let price = parseFloat(product.price.replace('$', ''))
 
   const cartItem = document.createElement('div');
   cartItem.classList.add('item');
@@ -145,8 +138,8 @@ const addToCart = (product) => {
         </div>
       `;
 
-  cartList.appendChild(cartItem);
-  updateTotals();
+  cartlist.appendChild(cartItem);
+  updatetotals();
 
   const plusBtn = cartItem.querySelector('.plus');
   const quantityValue = cartItem.querySelector('.quantity-value');
@@ -159,41 +152,41 @@ const addToCart = (product) => {
     e.preventDefault();
     quantity++;
     quantityValue.textContent = quantity;
-    itemTotal.textContent = `$${ price * quantity }`;
-     updateTotals();
+    itemTotal.textContent = `$${price * quantity}`;
+    updatetotals();
   })
 
   minusBtn.addEventListener('click', (e) => {
 
     e.preventDefault();
-    if(quantity > 1){
+    if (quantity > 1) {
       quantity--;
       quantityValue.textContent = quantity;
-      itemTotal.textContent = `$${ (price * quantity) }`;
-      updateTotals();
-    }else{
+      itemTotal.textContent = `$${(price * quantity)}`;
+      updatetotals();
+    } else {
       cartItem.classList.add('slide-out');
-      
-      setTimeout(()=>{
+
+      setTimeout(() => {
         cartItem.remove();
-      cartProduct = cartProduct.filter(item => item.id !== product.id);
-      updateTotals();
+        cartproduct = cartproduct.filter(item => item.id !== product.id);
+        updatetotals();
       }, 300)
     }
-    
-    
+
+
   })
 
 }
 
-const initApp = () => {
+const initapp = () => {
 
   fetch('product.json')
     .then(res => res.json())
     .then(data => {
-      productList = data;
-      showCards();
+      productlist = data;
+      showcards();
     })
 }
 
-initApp();
+initapp();
